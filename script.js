ead20047a115a7b761f1ab9437bd3ccf5ba68337
asmtransfer <mihalich9007@mail.ru>
2026-08-30 22:32:50 +0300

    Tech: убрать повторную загрузку CSS и обновить enhancements

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 (function(){
-  var css=document.createElement('link');
-  css.rel='stylesheet';
-  css.href='/brand-fixes.css?v=20260830-3';
-  document.head.appendChild(css);
-
   function loadEnhancements(){
     var e=document.createElement('script');
-    e.src='/site-enhancements.js?v=20260830-1';
+    e.src='/site-enhancements.js?v=20260830-4';
     e.defer=true;
     document.head.appendChild(e);
   }
